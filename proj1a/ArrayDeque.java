5b084158d9e0a25960c4c22affae6d3a6d8ee080
lyylyylyylyy <lyyzju@outlook.com>
2020-04-03 17:25:16 +0800

    project1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,4 +1,4 @@
-public class ArrayDeque<T>{
+public class ArrayDeque<T> {
     private static int initialCapacity = 8; // The starting size of your array should be 8.
     private int capacity; // The length of array
     private T[] items;
@@ -15,7 +15,7 @@ public class ArrayDeque<T>{
         size = 0;
     }
 
-    public int goFoward(int index){
+    private int goFoward(int index) {
         if (index==0){
             return capacity-1;
         } else {
@@ -24,23 +24,24 @@ public class ArrayDeque<T>{
     }
 
 
-    public int goBack(int index){
-        if (index==capacity-1){
+    private int goBack(int index) {
+        if (index == capacity-1){
             return 0;
         } else {
             return index+1;
         }
     }
-    public void resize(int newcapacity){
+    private void resize(int newcapacity) {
         T[] newItems = (T[]) new Object[newcapacity];
         int newFirst = nextFirst;
         int newLast = nextLast;
 
-        if (newFirst < newLast){
-            int length = newLast-newFirst+1;
-            System.arraycopy(items,newFirst,newItems,0,length);
-            nextFirst=length;
-            nextLast=newcapacity-1;
+        if (newFirst < newLast) {
+            int length = capacity-1;
+            System.arraycopy(items,newFirst+1,newItems,0,length);
+            System.arraycopy(items,0,newItems,length,newFirst+1);
+            nextFirst = 0;
+            nextLast = capacity-1;
         } else {
             int lenFirst = capacity-newFirst;
             int newCurrentFirst = newcapacity-lenFirst;
@@ -49,17 +50,17 @@ public class ArrayDeque<T>{
             System.arraycopy(items,0,newItems,0,lenLasts);
             nextFirst = newcapacity-lenFirst-1;
         }
-        capacity=newcapacity;
+        capacity = newcapacity;
         items = newItems;
     }
-    public void expand(){
-        if (size==capacity){
+    private void expand() {
+        if (size == capacity){
             int newcapacity = capacity*2;
             resize(newcapacity);
         }
     }
 
-    public void contract(){
+    private void contract() {
         double ratio = (double) size/capacity;
         if (ratio < 0.25){
             int newCapacity = capacity/2;
@@ -68,7 +69,7 @@ public class ArrayDeque<T>{
     }
 
     /*Adds an item of type T to the front of the deque.*/
-    public void addFirst(T item){
+    public void addFirst(T item) {
         items[nextFirst] = item;
         nextFirst = goFoward(nextFirst);
         size = size + 1;
@@ -76,7 +77,7 @@ public class ArrayDeque<T>{
     }
 
     /*Adds an item of type T to the back of the deque.*/
-    public void addLast(T item){
+    public void addLast(T item) {
         items[nextLast] = item;
         nextLast = goBack(nextLast);
         size = size + 1;
@@ -84,8 +85,8 @@ public class ArrayDeque<T>{
     }
 
     /*Returns true if deque is empty, false otherwise.*/
-    public boolean isEmpty(){
-        if (size==0){
+    public boolean isEmpty() {
+        if (size == 0){
             return true;
         } else {
             return false;
@@ -93,48 +94,50 @@ public class ArrayDeque<T>{
     }
 
     /*Returns the number of items in the deque.*/
-    public int size(){
+    public int size() {
         return size;
     }
 
     /*Prints the items in the deque from first to last, separated by a space.*/
-    public void printDeque(){
-        for (int i=0; i<capacity;i++){
+    public void printDeque() {
+        for (int i = 0; i<capacity; i++) {
             System.out.print(items[i] + " ");
         }
         System.out.println();
     }
 
     /*Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
-    public T removeFirst(){
+    public T removeFirst() {
         if (isEmpty()){
             return null;
         }
-        T removed = items[nextFirst];
-        items[nextFirst]=null;
-        nextFirst = goFoward(nextFirst);
+        T removed = items[nextFirst+1];
+        items[nextFirst+1]=null;
+        nextFirst = goBack(nextFirst);
         size = size-1;
+        contract();
         return removed;
     }
 
     /*Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
-    public T removeLast(){
+    public T removeLast() {
         if (isEmpty()){
             return null;
         }
-        T removed = items[nextLast];
-        items[nextLast]=null;
-        nextLast = goBack(nextLast);
+        T removed = items[nextLast-1];
+        items[nextLast-1] = null;
+        nextLast = goFoward(nextLast);
         size = size-1;
+        contract();
         return removed;
     }
 
     /*Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!*/
-    public T get(int index){
+    public T get(int index) {
         if (index >= size){
             return null;
         }
-        int currentIndex = nextFirst+1+index;
+        int currentIndex = nextFirst+index;
         if (currentIndex >= capacity){
             currentIndex = currentIndex - capacity;
         }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -7,12 +7,40 @@ public class ArrayDequeTest{
     @Test
     public void testAdd() {
         ArrayDeque<Integer> temp = new ArrayDeque<>();
-        temp.addFirst(0);
         temp.addLast(1);
-        int a = temp.get(0);
-        int b = temp.get(1);
-        assertEquals(0,a);
-        assertEquals(1,b);
+        temp.addLast(2);
+        temp.addLast(3);
+        temp.addLast(4);
+        temp.addLast(5);
+        temp.addLast(6);
+        temp.addLast(7);
+        temp.addLast(8);
+
+        int j = temp.get(0);
+        int a = temp.get(1);
+        int b = temp.get(2);
+        int c = temp.get(3);
+        int d = temp.get(4);
+        int e = temp.get(5);
+        int f = temp.get(6);
+        int g = temp.get(7);
+
+        assertEquals(2,a);
+        assertEquals(3,b);
+        assertEquals(4,c);
     }
 
+    @Test
+    public void testRemove() {
+        ArrayDeque<Integer> temp = new ArrayDeque<>();
+        temp.addFirst(1);
+        temp.addFirst(2);
+        temp.addFirst(3);
+        temp.addFirst(4);
+        temp.addFirst(5);
+        temp.addFirst(6);
+
+        int a = temp.removeFirst();
+        assertEquals(6, a);
+    }
 }
